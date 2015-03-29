43cad27864c7e4b01b03d0d6938f7da059e4d988
SencerH <sencerhamarat@gmail.com>
2015-03-29 21:06:41 +0300

    zcopy denemelerine devam

--- static/js/rbng.js
+++ static/js/rbng.js
@@ -6,25 +6,21 @@ $('#rabn').click(function(){
     $.get('/bird_name_requested/', {}, function(data){
         var bn = $('#bn');
         var sbn = $('#show_bird_name');
-        sbn.fadeOut('slow', function() {
+        sbn.fadeOut(function() {
             bn.html(data);
         });
         sbn.fadeIn();
     });
 });
 
-//ZeroClipboard.config( { swfPath: "/static/js/zcb/ZeroClipboard.swf" } );
-//
-//$('#show_bird_name').click(function () {
-//    var client = new ZeroClipboard( document.getElementById("bn") );
-//    client.on( "ready", function( readyEvent ) {
-//        alert( "ZeroClipboard SWF is ready!" );
-//        client.on( "aftercopy", function( event ) {
-//            `this` === `client`
-//            `event.target` === the element that was clicked
-//            event.target.style.display = "none";
-//            alert("Copied text to clipboard: " + event.data["text/plain"] );
-//        } );
-//    } );
-//});
+$("div#show_bird_name").zclip({
+    path:'{{ STATIC_URL }}js/ZeroClipboard.swf',
+    copy:$('h3#bn').text(),
+    beforeCopy:function(){
+        $('.info').fadeOut();
+    },
+    afterCopy:function(){
+        $('.check').fadeIn();
+    }
+});
 
