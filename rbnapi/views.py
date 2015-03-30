a8c19495fb4abcdbb90814d810c32568d261b6e6
Sencer H <recnes@recnes.com>
2015-03-30 16:35:05 +0300

    csrf protection added

--- rbnapi/views.py
+++ rbnapi/views.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from random import randint
-from django.core.context_processors import request
+from django import forms
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from django.views.decorators.csrf import csrf_protect
 from rbnapi.models import BirdNameDatabase
 
 
@@ -11,12 +12,24 @@ def generate_bird_name():
     count = BirdNameDatabase.objects.count() - 1
     index = randint(0, count)
     bn = BirdNameDatabase.objects.all()[index]
-    return bn.bird_name.title()
+    return bn.bird_name, bn.scientific_name.scientific_name
 
 
+class RBNForm(forms.Form):
+    sci_check = forms.BooleanField(required=False, label="Scientific Name", initial=False)
+
+
+@csrf_protect
 def bird_name_requested(request):
-    if request.method == 'GET':
-        return HttpResponse(generate_bird_name())
+    if request.method == 'POST':
+        form = RBNForm(request.POST)
+        if form.is_valid():
+            sci_check = form.cleaned_data['sci_check']
+
+            if sci_check:
+                return HttpResponse("{},{}".format(generate_bird_name()[0].title(),generate_bird_name()[1].title()))
+            else:
+                return HttpResponse(generate_bird_name()[0])
 
 
 class API():
