cd643710d5cf8f52327367d18fe6d9613e350d41
SencerH <sencerhamarat@gmail.com>
2015-04-03 22:45:34 +0300

    restfull api tekrar aktive edildi.

--- RandomBirdNameAPI/settings.py
+++ RandomBirdNameAPI/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'rest_framework',
+    'rest_framework',
     'rbnapi',
 )
 
