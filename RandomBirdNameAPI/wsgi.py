2122c45d54f0a12183bd625c87f3b3878f5173bb
SencerH <sencerhamarat@gmail.com>
2015-12-10 22:31:45 +0200

    wsgi fixed

--- RandomBirdNameAPI/wsgi.py
+++ RandomBirdNameAPI/wsgi.py
@@ -12,9 +12,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "RandomBirdNameAPI.settings")
 # os.environ["DJANGO_SETTINGS_MODULE"] = "RandomBirdNameAPI.settings"
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling, MediaCling
+from dj_static import Cling
 
-application = Cling(MediaCling(get_wsgi_application()))
+application = Cling(get_wsgi_application())
 # application = get_wsgi_application()
 
 
