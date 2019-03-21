004c8362ad68fafd950b6f5921bbe7cd9e3c6d0a
SencerH <sencerhamarat@gmail.com>
2019-03-21 18:17:58 +0300

    Merge Fix.

--- static/js/rbng.js
+++ static/js/rbng.js
@@ -2,24 +2,8 @@
  * Created by Sencer Hamarat on 28.03.2015.
  */
 
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
 $('#bird_name_form').submit(function(){
-    var csrftoken = getCookie('csrftoken');
+    var csrftoken = $("input[name=csrfmiddlewaretoken]").val();
     console.log(csrftoken);
     var sc = $('#get_scientific').prop('checked');
     var cn = $('#counter');
