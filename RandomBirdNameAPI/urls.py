006f8c64491f21b20b8c0ef7502b3645bd8802de
SencerH <sencerhamarat@gmail.com>
2019-03-21 16:18:47 +0300

    URL's are updated due to compatibility of Django2

--- RandomBirdNameAPI/urls.py
+++ RandomBirdNameAPI/urls.py
@@ -1,19 +1,19 @@
-from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.conf.urls import url, include
+from django.urls import path
+
 from rbnapi.views import (bird_name_requested, start_page)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'RandomBirdNameAPI.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # path('', 'RandomBirdNameAPI.views.home', name='home'),
+    # path('blog/', include('blog.paths')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    path('admin/', admin.site.urls),
 
-    url(r'^bird_name_requested/$', bird_name_requested, name='bird_name_requested'),
-    url(r'^$', start_page, name='index'),  # HTML Anasayfa
+    path('bird_name_requested/', bird_name_requested, name='bird_name_requested'),
+    path('', start_page, name='index'),  # HTML Index page
 
     # Wire up our API using automatic URL routing.
     # Additionally, we include login URLs for the browsable API.
