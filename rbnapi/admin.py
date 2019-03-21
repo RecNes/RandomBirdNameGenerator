3d4d389e7945dda139d84473c808cccb36d4a948
SencerH <sencerhamarat@gmail.com>
2019-03-21 16:17:37 +0300

    Admin registry method changed to original.

--- rbnapi/admin.py
+++ rbnapi/admin.py
@@ -1,46 +1,26 @@
 # -*- codign: utf-8 -*-
 from django.contrib import admin
+from django.contrib.admin import register
+
 from rbnapi.models import ScientificName, BirdNameDatabase, GeneralStatistics
 
 
+@register(BirdNameDatabase)
 class BirdNameDatabaseAdmin(admin.ModelAdmin):
     list_display = ['bird_name', 'scientific_name']
     ordering = ['bird_name']
     search_fields = ['bird_name']
 
 
+@register(ScientificName)
 class ScientificNameAdmin(admin.ModelAdmin):
     list_display = ['scientific_name']
     ordering = ['scientific_name']
     search_fields = ['scientific_name']
 
 
+@register(GeneralStatistics)
 class GeneralStatisticsAdmin(admin.ModelAdmin):
     list_display = ['created', 'client_ip', 'bird_name']
     ordering = ['created']
     search_fields = ['client_ip']
-
-
-def admin_register(admin_, namespace):
-    """
-        convenience function to easily register admin classes
-
-        :param admin: result of 'from django.contrib import admin'
-        :param namespace: must take a locally called globals
-
-        usage::
-
-            # should be at the end of the admin.py file
-            # globals must be called locally as below
-        admin_register(admin, namespace=globals())
-    """
-    for name, model_admin in namespace.copy().iteritems():
-        if name.endswith("Admin"):
-            model = namespace[name[:-5]]
-            try:
-                admin_.site.register(model, model_admin)
-            except:
-                raise
-
-
-admin_register(admin, namespace=globals())
