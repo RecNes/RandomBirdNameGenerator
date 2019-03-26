496538a674c3465770e691082ccb3523306d6c71
SencerH <sencerhamarat@gmail.com>
2019-03-26 15:43:32 +0300

    clipboard method changed

--- static/js/rbng.js
+++ static/js/rbng.js
@@ -2,37 +2,71 @@
  * Created by Sencer Hamarat on 28.03.2015.
  */
 
-
-$('#bird_name_form').submit(function(){
+$('#bird_name_form').submit(function () {
     var csrftoken = $.cookie('csrftoken');
     var sc = $('#get_scientific').prop('checked');
     var cn = $('#counter');
+    var bn_div = $('#bn_div');
     var bn = $('#bird_name');
+    var sn_div = $('#sn_div');
     var sn = $('#scientific_name');
     var sbn = $('#show_bird_name');
-    $.post('/bird_name_requested/', {'sci_check': sc, 'csrfmiddlewaretoken': csrftoken}, function(data){
-        sbn.fadeOut(function() {
+    $.post('/bird_name_requested/', {'sci_check': sc, 'csrfmiddlewaretoken': csrftoken}, function (data) {
+        sbn.fadeOut(function () {
             $('.check').hide();  // hide "copied to clipboard" message
-            sn.html('').hide();  // If checkbox unchecked clean object html and hide object
+            bn_div.hide();  // If checkbox unchecked clean object html and hide object
+            bn.html('');
+            sn_div.hide();  // If checkbox unchecked clean object html and hide object
+            sn.html('');
+
             var splited_data = data.split(",");
+
             bn.html(splited_data[0]);
-            if (sc){
-                sn.html(splited_data[1]).show();
+            bn_div.show();
+            if (sc) {
+                sn.html(splited_data[1]);
+                sn_div.show();
                 cn.html(splited_data[2]);
             } else {
                 cn.html(splited_data[1]);
             }
-            $('#copy-button').attr("data-clipboard-text", data).show();
+            $('#copy-button').show();
         });
         sbn.fadeIn();
     });
 });
 
-var client = new ZeroClipboard( document.getElementById("copy-button") );
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
 
-client.on( "ready", function( readyEvent ) {
-    client.on( "aftercopy", function( event ) {
-        $('#copy-button').hide();
-        $('.check').show();
-  });
-});
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        // console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(text) {
+    // console.log(text);
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text).then(function () {
+        // console.log('Async: Copying to clipboard was successful!');
+        var copied = $('.check');
+        copied.show();
+        copied.delay(750).fadeOut();
+
+    }, function (err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+}
