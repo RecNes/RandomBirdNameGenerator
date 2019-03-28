4cae6f949c928532a42ff6478491a15516a36c90
SencerH <sencerhamarat@gmail.com>
2019-03-28 15:41:16 +0300

    Models enhanced.

--- rbnapi/models.py
+++ rbnapi/models.py
@@ -1,5 +1,7 @@
 # coding: utf-8
+from django.core.signals import request_finished
 from django.db import models
+from django.dispatch import receiver
 
 
 class ScientificName(models.Model):
@@ -21,7 +23,19 @@ class BirdNameDatabase(models.Model):
 
 
 class GeneralStatistics(models.Model):
-    bird_name = models.ForeignKey(BirdNameDatabase, related_name='statistic', on_delete=models.CASCADE)
+    bird_name = models.ForeignKey("BirdNameDatabase", related_name='statistic', on_delete=models.CASCADE)
+    request_count = models.BigIntegerField(verbose_name="Count Number", default=0)
+    updated = models.DateTimeField(verbose_name=u"Last Update", auto_now=True)
+
+    def __unicode__(self):
+        return self.updated
+
+    class Meta:
+        ordering = ['updated', ]
+
+
+class RequestRecord(models.Model):
+    statistic = models.ForeignKey("GeneralStatistics", related_name='request_record', on_delete=models.CASCADE)
     client_ip = models.GenericIPAddressField(verbose_name=u"Client IP")
     created = models.DateTimeField(verbose_name=u"Request Date", auto_now_add=True)
 
@@ -30,3 +44,10 @@ class GeneralStatistics(models.Model):
 
     class Meta:
         ordering = ['created', ]
+
+
+@receiver(request_finished, sender=RequestRecord)
+def update_general_statistic(sender, environ, **kwargs):
+    print(environ)
+
+
