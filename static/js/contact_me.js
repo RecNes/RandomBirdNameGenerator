042ec61a5cd0d28a22ce6be62a489a9a41010382
SencerH <sencerhamarat@gmail.com>
2019-03-28 11:36:34 +0300

    undefined bugfix.

--- static/js/contact_me.js
+++ static/js/contact_me.js
@@ -14,7 +14,7 @@ $(function() {
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
+            if (!firstName == 'undefined' && firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
