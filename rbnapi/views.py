9307d1499fb6f4fbf6e2193efadd51965a05e59b
SencerH <sencerhamarat@gmail.com>
2019-03-28 15:41:58 +0300

    classic view removed due to implementing rest

--- rbnapi/views.py
+++ rbnapi/views.py
@@ -1,50 +1,52 @@
 # coding: utf-8
+import logging
 from random import randint
 
-from django import forms
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from rbnapi.models import BirdNameDatabase, GeneralStatistics
-from rbnapi.rb_logger import log
+from .serializers import BirdNameSerializer
+
+log = logging.getLogger(__name__)
+
+class GetBirdName(APIView):
+
+    def save_general_statistics(self, request, bn):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            client_ip = x_forwarded_for.split(',')[0]
+        else:
+            client_ip = request.META.get('REMOTE_ADDR')
+        GeneralStatistics(bird_name=bn, client_ip=client_ip).save()
+        return GeneralStatistics.objects.count()
+
+    def get(self, request):
+        count = BirdNameDatabase.objects.count() - 1
+        index = randint(0, count)
+        bn = BirdNameDatabase.objects.all()[index]
+        serialized = BirdNameSerializer(bn, many=False)
+        count = self.save_general_statistics(request, bn)
+        log.info("{} / {}".format(bn.bird_name, bn.scientific_name.scientific_name))
+        return Response(serialized.data)
 
 
-def save_general_statistics(request, bn):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        client_ip = x_forwarded_for.split(',')[0]
-    else:
-        client_ip = request.META.get('REMOTE_ADDR')
-    GeneralStatistics(bird_name=bn, client_ip=client_ip).save()
-    return GeneralStatistics.objects.count()
+# class RBNForm(forms.Form):
+#     sci_check = forms.BooleanField(required=False, label="Scientific Name", initial=False)
 
 
-def generate_bird_name(request):
-    count = BirdNameDatabase.objects.count() - 1
-    index = randint(0, count)
-    bn = BirdNameDatabase.objects.all()[index]
-    count = save_general_statistics(request, bn)
-    log.info("{} / {}".format(bn.bird_name, bn.scientific_name.scientific_name))
-    return bn.bird_name, bn.scientific_name.scientific_name, count
-
-
-class RBNForm(forms.Form):
-    sci_check = forms.BooleanField(required=False, label="Scientific Name", initial=False)
-
-
-@csrf_protect
-# @csrf_exempt
-def bird_name_requested(request):
-    if request.method == 'POST':
-        form = RBNForm(request.POST)
-        if form.is_valid():
-            sci_check = form.cleaned_data['sci_check']
-            bn = generate_bird_name(request)
-            if sci_check:
-                return HttpResponse("{},{},{}".format(bn[0].title(), bn[1].title(), bn[2]))
-            else:
-                return HttpResponse("{},{}".format(bn[0].title(), bn[2]))
+# @csrf_protect
+# def bird_name_requested(request):
+#     if request.method == 'POST':
+#         form = RBNForm(request.POST)
+#         if form.is_valid():
+#             sci_check = form.cleaned_data['sci_check']
+#             bn = generate_bird_name(request)
+#             if sci_check:
+#                 return HttpResponse("{},{},{}".format(bn[0].title(), bn[1].title(), bn[2]))
+#             else:
+#                 return HttpResponse("{},{}".format(bn[0].title(), bn[2]))
 
 
 def start_page(request, title="Random Bird Name Generator"):
