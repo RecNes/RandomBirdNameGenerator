54c7a8f63aeab26e2f6083e67a29614ee052de26
Sencer H <sencerhamarat@gmail.com>
2020-04-14 16:01:14 +0300

    Fox for " and ' chars

--- rbntools/secretgen.py
+++ rbntools/secretgen.py
@@ -1,3 +1,3 @@
 import string, random
-uni=string.ascii_letters+string.digits+string.punctuation
-print(repr(''.join([random.SystemRandom().choice(uni) for i in range(random.randint(45,50))])))
+uni = string.ascii_letters + string.digits + string.punctuation
+print(repr("".join([random.SystemRandom().choice(uni) for i in range(random.randint(45,50))])))
