ef48c857882ba755082594c65064f1a6f814ae41
SencerH <sencerhamarat@gmail.com>
2022-07-15 21:08:18 +0300

    Add request limit check per IP

--- rbnapi/views.py
+++ rbnapi/views.py
@@ -1,8 +1,11 @@
 # coding: utf-8
 import logging
+from datetime import timedelta
 from random import randint
 
+import rest_framework.status
 from django.shortcuts import render
+from django.utils import timezone
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -18,13 +21,25 @@ class GetBirdName(APIView):
     """
 
     @staticmethod
-    def save_general_statistics(request, bn):
+    def extract_ip_from(request):
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
             client_ip = x_forwarded_for.split(',')[0]
         else:
             client_ip = request.META.get('REMOTE_ADDR')
 
+        return client_ip
+
+    @staticmethod
+    def check_request_limit(client_ip):
+        end_date = timezone.now()
+        start_date = end_date - timedelta(minutes=1)
+        request_count = RequestRecord.objects.filter(
+            client_ip=client_ip, created__gte=start_date, created__lte=end_date
+        ).count()
+        return request_count >= 10
+
+    def save_general_statistics(self, client_ip, bn):
         gs, created = GeneralStatistics.objects.get_or_create(bird_name=bn)
         gs.request_count += 1
         gs.save()
@@ -32,11 +47,15 @@ class GetBirdName(APIView):
         return GeneralStatistics.objects.count()
 
     def get(self, request):
+        client_ip = self.extract_ip_from(request)
+        is_limited = self.check_request_limit(client_ip)
+        if is_limited:
+            return Response({}, status=rest_framework.status.HTTP_403_FORBIDDEN)
         count = BirdNameDatabase.objects.count() - 1
         index = randint(0, count)
         bn = BirdNameDatabase.objects.all()[index]
         serialized = BirdNameSerializer(bn, many=False)
-        self.save_general_statistics(request, bn)
+        self.save_general_statistics(client_ip, bn)
         log.info("{} / {}".format(bn.bird_name, bn.scientific_name.scientific_name))
         return Response(serialized.data)
 
