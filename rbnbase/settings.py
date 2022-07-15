1f503b20e20fcf1ee6b37f662c2058d03c664f8b
SencerH <sencerhamarat@gmail.com>
2022-07-15 15:57:47 +0300

    Change static root settings and re-collect statics.

--- rbnbase/settings.py
+++ rbnbase/settings.py
@@ -150,11 +150,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
--- rbnbase/urls.py
+++ rbnbase/urls.py
@@ -4,16 +4,8 @@ from django.urls import path
 from rbnapi import views
 
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    # Examples:
-    # path('', 'rbnbase.views.home', name='home'),
-    # path('blog/', include('blog.paths')),
-
     path('admin/', admin.site.urls),
-
     path('bnapi/', views.GetBirdName.as_view()),
-    # path('bird_name_requested', views.bird_name_requested, name='bird_name_requested'),
-    path('', views.start_page, name='index'),  # HTML Index page
+    path('', views.start_page, name='index'),
 ]
