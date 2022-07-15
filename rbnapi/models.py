6b66a7879c44e702beee349f365b1fc8bde428f9
SencerH <sencerhamarat@gmail.com>
2022-07-15 15:55:43 +0300

    change model __unicode__ methods with __str__

--- rbnapi/models.py
+++ rbnapi/models.py
@@ -1,11 +1,12 @@
 # coding: utf-8
+from django.conf import settings
 from django.db import models
 
 
 class ScientificName(models.Model):
     scientific_name = models.TextField(verbose_name=u"Scientific Name", unique=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.scientific_name
 
 
@@ -13,7 +14,7 @@ class BirdNameDatabase(models.Model):
     scientific_name = models.ForeignKey(ScientificName, related_name='bird_name', on_delete=models.CASCADE)
     bird_name = models.TextField(verbose_name=u"Bird Name", unique=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.bird_name
 
     class Meta:
@@ -25,8 +26,8 @@ class GeneralStatistics(models.Model):
     request_count = models.BigIntegerField(verbose_name="Count Number", default=0)
     updated = models.DateTimeField(verbose_name=u"Last Update", auto_now=True)
 
-    def __unicode__(self):
-        return self.updated
+    def __str__(self):
+        return self.updated.strftime(settings.DATETIME_FORMAT) if self.updated else ""
 
     class Meta:
         ordering = ['updated', ]
@@ -37,8 +38,8 @@ class RequestRecord(models.Model):
     client_ip = models.GenericIPAddressField(verbose_name=u"Client IP")
     created = models.DateTimeField(verbose_name=u"Request Date", auto_now_add=True)
 
-    def __unicode__(self):
-        return self.created
+    def __str__(self):
+        return self.created.strftime(settings.DATETIME_FORMAT) if self.created else ""
 
     class Meta:
         ordering = ['created', ]
