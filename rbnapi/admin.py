9acf70869a0e8b9eb8d63f554f9c99888a5b7b58
SencerH <sencerhamarat@gmail.com>
2022-07-19 23:10:27 +0300

    Admin page enhancement

--- rbnapi/admin.py
+++ rbnapi/admin.py
@@ -1,25 +1,56 @@
 # -*- codign: utf-8 -*-
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.admin import register
 
-from rbnapi.models import ScientificName, BirdNameDatabase, GeneralStatistics
+from rbnapi.models import RequestRecord, ScientificName, BirdNameDatabase, GeneralStatistics
 
 
 @register(BirdNameDatabase)
 class BirdNameDatabaseAdmin(admin.ModelAdmin):
-    list_display = ['bird_name', 'scientific_name']
+    list_display = ['formatted_bird_name', 'scientific_name']
     ordering = ['bird_name']
     search_fields = ['bird_name']
+
+    def formatted_bird_name(self, obj):
+        return obj.bird_name.title()
+
+    formatted_bird_name.admin_order_field = 'bird_name'
+    formatted_bird_name.short_description = 'Bird Name'
 
 
 @register(ScientificName)
 class ScientificNameAdmin(admin.ModelAdmin):
-    list_display = ['scientific_name']
+    list_display = ['formatted_scientific_name']
     ordering = ['scientific_name']
     search_fields = ['scientific_name']
+
+    def formatted_scientific_name(self, obj):
+        return obj.scientific_name.title()
+
+    formatted_scientific_name.admin_order_field = 'scientific_name'
+    formatted_scientific_name.short_description = 'Scientific Name'
 
 
 @register(GeneralStatistics)
 class GeneralStatisticsAdmin(admin.ModelAdmin):
-    list_display = ['bird_name', 'request_count', 'updated']
+    list_display = ['bird_name', 'request_count', 'formatted_updated']
     ordering = ['-updated']
+
+    def formatted_updated(self, obj):
+        return obj.updated.strftime(settings.DATETIME_FORMAT)
+
+    formatted_updated.admin_order_field = 'created'
+    formatted_updated.short_description = 'Last Update'
+
+
+@register(RequestRecord)
+class RequestRecordAdmin(admin.ModelAdmin):
+    list_display = ['client_ip', 'statistic', 'formatted_created']
+    ordering = ['-created']
+
+    def formatted_created(self, obj):
+        return obj.created.strftime(settings.DATETIME_FORMAT)
+
+    formatted_created.admin_order_field = 'created'
+    formatted_created.short_description = 'Request Date'
